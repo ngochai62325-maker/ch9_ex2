bf1d65c68d7db376aedba968c29900c5a8cb532f
oceanking12312 <ngochai62325@gmail.com>
2025-10-01 22:15:39 +0700

    feat: fix add item

--- src/main/java/murach/business/Cart.java
+++ src/main/java/murach/business/Cart.java
@@ -25,7 +25,7 @@ public class Cart implements Serializable {
         for (int i = 0; i < items.size(); i++) {
             LineItem lineItem = items.get(i);
             if (lineItem.getProduct().getCode().equals(code)) {
-                lineItem.setQuantity(quantity);
+                lineItem.setQuantity(quantity + lineItem.getQuantity());
                 return;
             }
         }
